b1c0796a3090c7eb9ef0d9eaa205348c235cc43b
Kasia Vasiluk <kasia.vasiluk@gmail.com>
2018-02-13 14:02:43 +0100

    Add order.js script for designed forms.

---
+++ scripts/order.js
@@ -0,0 +1,10 @@
+(function (window) {
+  'use-strict';
+  var Application = window.Application || {};
+  function ToOrder() {
+
+  }
+
+  Application.ToOrder = ToOrder;
+  window.Application = Application;
+})(window);
