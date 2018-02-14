e70322e628922e4a18ee1d7216341e0d162f6b10
Kasia Vasiluk <kasia.vasiluk@gmail.com>
2018-02-14 11:52:22 +0100

    Finish basic possibility of placing an order.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 (function(window) {
   'use strict';
+  var SELECTOR_FORM = '[data-order="formular"]';
   var Application = window.Application;
   var Barista = Application.Barista;
   var DataBase = Application.DataBase;
+  var ToOrder = Application.ToOrder;
   var myBarista = new Barista('#23, John', new DataBase());
   window.myBarista = myBarista;
+  var toOrder = new ToOrder(SELECTOR_FORM);
+  toOrder.addSubmitHandler(
+    myBarista.addOrder.bind(myBarista));
+  console.log(toOrder);
 })(window);
--- scripts/order.js
+++ scripts/order.js
@@ -1,9 +1,34 @@
 (function (window) {
   'use-strict';
   var Application = window.Application || {};
-  function ToOrder() {
+  var $ = window.jQuery;
 
+  function ToOrder(selector) {
+    if(!selector) {
+      throw new Error('No selector');
+    }
+    this.$formElement = $(selector);
+    if(this.$formElement === 0) {
+      throw new Error('No proper elements: ' + selector);
+    }
   }
+
+  ToOrder.prototype.addSubmitHandler = function(fn) {
+    console.log('Subimt handler');
+    this.$formElement.on('submit', function(click) {
+      click.preventDefault();
+      var data = {};
+       $(this).serializeArray().forEach(function(element) {
+        data[element.name] = element.value;
+        console.log('Element ' + element.name + 'Value: ' + element.value);
+      });
+      console.log(data);
+      fn(data);
+      this.reset();
+      this.elements[0].focus();
+    });
+  };
+
 
   Application.ToOrder = ToOrder;
   window.Application = Application;
